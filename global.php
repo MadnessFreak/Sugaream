fb666bc041508d58c35bc3cfb2510f00812cb09e
MadnessFreak <supervivo@icloud.com>
2014-10-30 15:22:34 +0100

    Create global.php

---
+++ global.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Sugaream Framework
+ *
+ * @author		MadnessFreak
+ * @copyright	2014 MadnessFreak
+ *
+ */
+
+// include config
+require_once(dirname(__FILE__).'/config.inc.php');
+
+// include app
+require_once(RELATIVE_SYS_DIR.'system/System.class.php');
+
+// initiate wcf core
+new framework\system\System();
