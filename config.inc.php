3e1dcd30806f0bbfe4bcd1ad31e3b054ff12180d
MadnessFreak <supervivo@icloud.com>
2014-10-30 15:23:22 +0100

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+// app
+if (!defined('APP_DIR')) define('APP_DIR', dirname(__FILE__).'/');
+if (!defined('RELATIVE_APP_DIR')) define('RELATIVE_APP_DIR', '');
+
+if (!defined('SYS_DIR')) define('SYS_DIR', dirname(__FILE__).'/framework/');
+if (!defined('RELATIVE_SYS_DIR')) define('RELATIVE_SYS_DIR', RELATIVE_APP_DIR.'framework/');
